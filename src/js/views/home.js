4895cd012032ea727e642e337486425766ef1cac
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2024-08-14 08:47:53 +0000

    Proyecto finalizado

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "../../styles/home.css";
-import Agenda from "./Agenda";
+import Agenda from "./agenda";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<Agenda/>
-	</div>
+  <div className="text-center mt-5">
+    <Agenda />
+  </div>
 );
